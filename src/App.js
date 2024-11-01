1d5786c4b1b9d1cb571d935068001b25bae46cae
TerichP <petr.terich@gmail.com>
2024-11-01 23:09:54 +0100

    added partial code for infinite loading MyCard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import { useState } from 'react';
 import './App.css';
 import MyCard from './components/MyCard';
 import MyNav from './components/MyNav';
+import InfiniteScroll from "react-infinite-scroll-component"
 
 function App() {
+  const [dataSource, setDataSource] = useState(MyCard)
   return (
     <div className="App">
-      <MyNav 
+    {/*  <MyNav 
         eshop="Nazev eshopu">
       </MyNav>
 
@@ -15,8 +18,22 @@ function App() {
         price="3000" 
         left="3" 
         alt="headphones" 
-        path=" ZDE PATH NA OBRAZEK "
-      />
+        path=" ZDE PATH NA OBRAZEK " 
+      /> */}
+
+      <InfiniteScroll dataLength={dataSource.length}>
+        {dataSource.map((item, index)=>{
+          return <MyCard
+            name = {index+1}
+            type = {index+1}
+            price = {index+1}
+            left = {index+1}
+            alt = {index+1}          
+          ></MyCard>
+        })}
+
+
+      </InfiniteScroll>
     </div>
   );
 }
