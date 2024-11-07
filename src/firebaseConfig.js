cdbb32bc28d6a1b803db96f7a9145204184bfd57
TerichP <petr.terich@gmail.com>
2024-11-07 15:51:38 +0100

    edited fibaseConfig to represent env values

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -7,14 +7,14 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB8uZSXmO7yy_bawH-GsvbPMo3oOXWEwZE",
-  authDomain: "keramika-8bd77.firebaseapp.com",
-  databaseURL: "https://keramika-8bd77-default-rtdb.europe-west1.firebasedatabase.app/",
-  projectId: "keramika-8bd77",
-  storageBucket: "keramika-8bd77.firebasestorage.app",
-  messagingSenderId: "314768701339",
-  appId: "1:314768701339:web:e5709b9097f1a13262533e",
-  measurementId: "G-KY0GG4PV4F"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,  
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
