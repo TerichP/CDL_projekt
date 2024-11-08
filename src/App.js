0b1e1280ccd66647e8a334be860b7d7435dca56b
TerichP <petr.terich@gmail.com>
2024-11-08 20:10:53 +0100

    added Read.js file for simplified reading of data saved on the database, added routing for Read

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Container, Grid2 } from '@mui/material';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Write from './components/Write';
 import ProductLoad from './components/ProductLoad';
-
+import Read from './components/Read';
 
 function App() {
   return (
@@ -14,6 +14,7 @@ function App() {
         <Routes>
           <Route path ="/"/>
           <Route path ="/write" element={<Write/>}/>
+          <Route path ="/read" element={<Read/>}/>
         </Routes>
       </Router>
       
---
+++ src/components/Read.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import app from "../firebaseConfig";
+import { getDatabase, ref, get } from "firebase/database";
+
+function Read() {
+
+    let [productArray, setProductArray] = useState([]);
+
+    const fetchData = async () => {
+        const db = getDatabase(app);
+        const dbRef = ref(db, "shop/products");
+        const snapshot = await get(dbRef);
+        if(snapshot.exists()) {
+            setProductArray(Object.values(snapshot.val()));
+        } else {
+            alert("error");
+        }
+    }
+
+    return (
+        <div>
+            <button onClick={fetchData}>DISPLAY DATA</button>
+            <ul>
+                {productArray.map((item, index) => (
+                    <li key={index}>
+                        {item.productAlt}:{item.productPrice}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+    export default Read
