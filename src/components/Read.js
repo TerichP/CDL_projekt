13082a9a5e75db1eb278927a2cb146955d5becab
TerichP <petr.terich@gmail.com>
2024-11-08 17:29:40 +0100

    WIP read component

--- src/components/Read.js
+++ src/components/Read.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+import app from "../firebaseConfig";
+import { getDatabase, ref, get } from "firebase/database";
 
 function Read() {
+
+    let [productArray, setProductArray] = useState([]);
+
+    const fetchData = async () => {
+        const db = getDatabase(app);
+        const dbRef = ref(db, "shop/products");
+        const snapshot = await get(dbRef);
+        if(snapshot.exists()) {
+            setProductArray(Object.values(snapshot.val()));
+        } else {
+            alert("error");
+        }
+    }
+
     return (
-        <div>Read</div>
+        <div>
+            <button onClick={fetchData}>DISPLAY DATA</button>
+        </div>
     )
 }
 
